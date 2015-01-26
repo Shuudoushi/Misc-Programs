475de8169c30dba1a08063c67fff768f73a37a11
Shuudoushi <shuudoushi@pc-logix.com>
2015-01-25 19:25:32 -0600

    Added minor turbine support and update support.

--- BR_Reactor.lua
+++ BR_Reactor.lua
@@ -1,17 +1,22 @@
 local component = require("component")
 local computer = require("computer")
 local string = require("string")
+local t1 = component.br_turbine
 local r1 = component.br_reactor
 local term = require("term")
 local os = require("os")
 local event = require("event")
 local kb = require("keyboard")
 local unicode = require("unicode")
+local shell = require("shell")
 local running = true
 local X = r1.getNumberOfControlRods()
 local coolantType = r1.getCoolantType()
 local hotFluidType = r1.getHotFluidType()
 local commandInput = ""
+local refresh = 1
+local power = r1.getEnergyProducedLastTick()
+local energyBuffer = r1.getEnergyStored()
 
 local function round(num, idp)
 	local mult = 10^(idp or 0)
@@ -47,12 +52,15 @@ local function activeCool()
 	end
 
 	if r1.isActivelyCooled() == true then
-		print("Max Coolant Amount: " .. r1.getCoolantAmountMax())
-		print("Current Coolant Amount: " .. round(r1.getCoolantAmount(), 2))
+		print("Rotor Speed: " .. round(t1.getRotorSpeed(), 3) .. " RPM")
+		print("Max Coolant Amount: " .. r1.getCoolantAmountMax() .. " mB")
+		print("Current Coolant Amount: " .. round(r1.getCoolantAmount(), 2) .. " mB")
 		print("Coolant: " .. coolantType)
 		print("Max Hot Fluid: " .. r1.getHotFluidAmountMax())
 		print("Current Hot Fluid: " .. round(r1.getHotFluidAmount(), 2))
 		print("Hot Fluid Type: " .. hotFluidType)
+		power = t1.getEnergyProducedLastTick()
+		energyBuffer = t1.getEnergyStored()
 	end
 end
 
@@ -72,24 +80,15 @@ local function ui()
 	term.write(status)
 	component.gpu.setForeground(0xFFFFFF)
 	term.setCursor(1,2)
-	term.write("Core Temp: " .. round(r1.getFuelTemperature(), 2) .. " " .. unicode.char(248) .. "C")
-	term.setCursor(1,3)
-	term.write("Case Temp: " .. round(r1.getCasingTemperature(), 2) .. " " .. unicode.char(248) .. "C")
-	term.setCursor(1,4)
-	term.write("Energy Buffer: " .. round(r1.getEnergyStored(), 2) .. " RF")
-	term.setCursor(1,5)
-	term.write("Energy Output: " .. round(r1.getEnergyProducedLastTick(), 2) .. " RF/t")
-	term.setCursor(1,6)
-	term.write("Fuel Reactivity: " .. round(r1.getFuelReactivity(), 2) .. "%")
-	term.setCursor(1,7)
-	term.write("Max Fuel: " .. round(r1.getFuelAmountMax(), 2) .. " mB")
-	term.setCursor(1,8)
-	term.write("Remaining Fuel: " .. round(r1.getFuelAmount(), 2) .. " mB")
-	term.setCursor(1,9)
-	term.write("Fuel Consumtion: " .. round(r1.getFuelConsumedLastTick(), 3) .. " mB/t")
-	term.setCursor(1,10)
-	term.write("Waste: " .. round(r1.getWasteAmount(), 2) .. " mB")
-	term.setCursor(1,11)
+	print("Core Temp: " .. round(r1.getFuelTemperature(), 2) .. " " .. unicode.char(248) .. "C")
+	print("Case Temp: " .. round(r1.getCasingTemperature(), 2) .. " " .. unicode.char(248) .. "C")
+	print("Energy Buffer: " .. round(energyBuffer, 2) .. " RF")
+	print("Energy Output: " .. round(power, 2) .. " RF/t")
+	print("Fuel Reactivity: " .. round(r1.getFuelReactivity(), 2) .. "%")
+	print("Max Fuel: " .. round(r1.getFuelAmountMax(), 2) .. " mB")
+	print("Remaining Fuel: " .. round(r1.getFuelAmount(), 2) .. " mB")
+	print("Fuel Consumtion: " .. round(r1.getFuelConsumedLastTick(), 3) .. " mB/t")
+	print("Waste: " .. round(r1.getWasteAmount(), 2) .. " mB")
 	activeCool()
 	rodsList()
 end
@@ -120,11 +119,13 @@ local function inputKey()
 	term.write("start - Starts the reactor.")
 	term.setCursor(65,6)
 	term.write("setAll <0~100> - Sets all control rods to X.")
+	term.setCursor(1,7)
+	term.write("update - Updates the program, the program must be manually restarted")
 	term.setCursor(10,1)
 end
 
 local function userInput()
-	_, _, _, c = event.pull(0.5, "key_down")
+	_, _, _, c = event.pull(refresh, "key_down")
 
 	if c == kb.keys.enter or c == kb.keys.numpadenter then
 	term.clear()
@@ -172,6 +173,17 @@ end
   	output = ""
   	commandInput = ""
   	ui()
+  end
+
+  if commandInput == "update" then
+  	shell.execute("wget -fq https://raw.githubusercontent.com/Shuudoushi/Misc-Programs/master/BR_Reactor.lua reactor.lua")
+  	term.clear()
+  	term.setCursor(1,1)
+  	print("Program closing to finsih update, please restart it.")
+  	os.sleep(2.5)
+  	term.clear()
+  	term.setCursor(1,1)
+  	running = false
   end
 end
 
